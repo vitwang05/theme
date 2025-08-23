a4ec9b2dd8fa1feb79b11a124bd98e050dbe7a26
PKshift05 <quang05012003@gmail.com>
2025-08-24 00:41:56 +0700

    add page

--- functions.php
+++ functions.php
@@ -9,3 +9,33 @@ function simple_bootstrap_theme_load_script() {
 }
 
 add_action("wp_enqueue_scripts","simple_bootstrap_theme_load_script");
+
+function simple_bootstrap_theme_nav_config(){
+
+    register_nav_menus(array(
+        "sbt_primary_menu_id" => "SBT Primary Menu(Top Menu)",
+        "sbt_secondary_menu_id" => "SBT Sidebar"
+    ));
+
+}
+
+
+add_action("after_setup_theme","simple_bootstrap_theme_nav_config");
+
+
+function simple_bootstrap_theme_add_li_class($classes, $item, $args){
+    $classes[] = "nav-item";
+    return $classes;
+}
+
+
+add_filter("nav_menu_css_class", "simple_bootstrap_theme_add_li_class", 1, 3);
+
+
+function simple_bootstrap_theme_add_anchor_links($classes, $item, $args){
+    $classes['class'] = "nav-link";
+    return $classes;
+}
+
+
+add_filter("nav_menu_link_attributes", "simple_bootstrap_theme_add_anchor_links", 1, 3);
--- header.php
+++ header.php
@@ -20,7 +20,14 @@
                 <a class="navbar-brand" href="#!">Start Bootstrap</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <?php 
+                        wp_nav_menu(array(
+                            "theme_location" => "sbt_primary_menu_id",
+                            "container" => false,
+                            "items_wrap" => '<ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">%3$s</ul>'
+                        ));
+                    ?>
+                    <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
                         <li class="nav-item dropdown">
@@ -32,7 +39,7 @@
                                 <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                             </ul>
                         </li>
-                    </ul>
+                    </ul> -->
                     <form class="d-flex">
                         <button class="btn btn-outline-dark" type="submit">
                             <i class="bi-cart-fill me-1"></i>
